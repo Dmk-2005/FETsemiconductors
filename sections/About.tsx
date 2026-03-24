c23176ec76e58612d77cea621dc50f5ca9932b20
Mohan Kiran Dongara <mohankirandongara143@gmail.com>
2026-03-24 17:07:41 +0530

    updated homepage content

--- sections/About.tsx
+++ sections/About.tsx
@@ -2,5 +2,9 @@ import Link from "next/link";
 import FadeIn from "@/components/FadeIn";
 
 export default function About() {
-   
+  return (
+    <section className="">
+      
+    </section>
+  );
 }
