8843a8528c4647b28483b61aad741368e88f986d
Mohan Kiran Dongara <mohankirandongara143@gmail.com>
2026-05-09 02:19:52 +0530

    fix update

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import { GoogleAnalytics } from "@next/third-parties/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
